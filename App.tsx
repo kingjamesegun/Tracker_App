6ae37fb0b40ad4791585d34942bd0d1d51bcb376
James <kingjamesegun@gmail.com>
2022-07-06 19:06:09 +0100

    Tab added

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+	return (
+    <View>
+      <Text>Nice oner</Text>
     </View>
-  );
+		// <NavigationContainer>
+		// 	<Tab.Navigator initialRouteName='Home' >
+		// 		<Tab.Screen name='Home' component={HomeScreen} />
+		// 	</Tab.Navigator>
+		// </NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
